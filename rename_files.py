79a358033758b5c0179046523a572378e1dca2a9
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-09 17:14:59 +0100

    Ad  for loop

---
+++ rename_files.py
@@ -0,0 +1,10 @@
+import os
+def rename_files():
+    # (1) get file names from a folder
+    file_list = os.listdir('/Users/ndeyisatoujobe/Desktop/udacity/Programming_Foundations_with_Python/file_rename/prank')
+    print (file_list)
+    # (2) for each file, rename filename
+    for file_name in file_list:
+        os.rename(*, )
+
+rename_files()
