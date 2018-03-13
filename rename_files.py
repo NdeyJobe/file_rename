9f4480f34a95acdcedceacbb471728a40b035e9b
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-13 11:11:29 +0100

    Add file path

--- rename_files.py
+++ rename_files.py
@@ -1,11 +1,11 @@
 import os
 def rename_files():
     # (1) get file names from a folder
-    file_list = os.listdir('/Users/ndeyisatoujobe/Desktop/udacity/Programming_Foundations_with_Python/file_rename/prank')
+    file_list = os.listdir('FILE_PATH')
     #print (file_list)
     saved_path = os.getcwd()
     #print("Current Working Directory is "+saved_path)
-    os.chdir('/Users/ndeyisatoujobe/Desktop/udacity/Programming_Foundations_with_Python/file_rename/prank')
+    os.chdir('FILE_PATH')
     # (2) for each file, rename filename
     for file_name in file_list:
         print('Old Name - ' + file_name)
