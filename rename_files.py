9e0a071ea68fde5c1503cc032880b423499b3c35
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-13 11:02:29 +0100

    Add rename loop

--- rename_files.py
+++ rename_files.py
@@ -2,9 +2,14 @@ import os
 def rename_files():
     # (1) get file names from a folder
     file_list = os.listdir('/Users/ndeyisatoujobe/Desktop/udacity/Programming_Foundations_with_Python/file_rename/prank')
-    print (file_list)
+    #print (file_list)
+    saved_path = os.getcwd()
+    #print("Current Working Directory is "+saved_path)
+    os.chdir('/Users/ndeyisatoujobe/Desktop/udacity/Programming_Foundations_with_Python/file_rename/prank')
     # (2) for each file, rename filename
     for file_name in file_list:
-        os.rename(*, )
-
+        print('Old Name - ' + file_name)
+        os.rename(file_name, file_name.translate(None,'0123456789'))
+        print('New Name - ' + file_name.translate(None,'0123456789'))
+    os.chdir (saved_path)          
 rename_files()
